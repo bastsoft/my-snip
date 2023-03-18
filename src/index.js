6fe90fa49d876519bd7f386451fd987f74204f20
p.laptev <p.laptev@i.krediska.ru>
2023-03-18 12:03:35 +0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,33 +1,3 @@
-import importGistId from "./lib/import-gist.js"
-import Queue from "./lib/commands-queue.js";
-import getCy from "./lib/cy.js";
+import snip from "./snip.js";
 
-import createMenu from "./lib/create-menu.js";
-import fetchjsonp from "./lib/fetchjsonp.js";
-
-const cy = getCy(Queue);
-
-Object.defineProperty(window, "cy", {
-    get: function () {
-        cy.initEl(document);
-
-        return cy;
-    },
-    configurable: true
-});
-
-export default {
-  load(url){
-    import(url).then((res)=>{
-      const snippets = res.default;
-      createMenu(snippets);
-    });
-  },
-  loadGist(data){
-    importGistId(fetchjsonp, document, data.id).then(gists => {
-      const js = encodeURIComponent(gists[data.file].text);
-  
-      this.load('data:text/javascript;charset=utf-8,' + js);
-    });
-  },
-};
+window.snip = snip;
